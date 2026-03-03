5a6d4b2af057bcdb5e5fb3e023615d5f37e6e6cb
Tobiel Lucas <tobiel.lucas@gmail.com>
2026-03-03 22:34:41 +0800

    Initial Next.js portfolio setup

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            Tobiel's Portfolio 2026
+          </h1>
+          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+            Senior UX Researcher & Systems-Oriented Product Strategist.
+            I turn ambiguity into scalable design and research systems,
+            balancing business goals with human-centered execution.{" "}
+          </p>
+        </div>
+        
+      </main>
+    </div>
+  );
+}
